f62d03c6fff3b42b3dff8559599a053c45b42115
Divya <beingdivyadixit@gmail.com>
2022-12-07 18:38:53 +0530

    [Divya] Addition of Two Numbers

---
+++ ConsoleApp1/calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class calculator
+    {
+       public int AddTwoNumbers(int firstNumber,int secondNumber)
+        {   
+            int result=firstNumber + secondNumber;
+            Console.WriteLine("Sum Of Two Numbers {0} ",result);
+            return result;
+        }
+   }
+}
